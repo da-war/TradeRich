674d66eac57863bf59508a4efd383924cb635cbe
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-08 18:39:11 +0500

    done with set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +17,14 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    AfBold: require("../assets/fonts/AfacadFlux-Bold.ttf"),
+    AfMedium: require("../assets/fonts/AfacadFlux-Medium.ttf"),
+    AfRegular: require("../assets/fonts/AfacadFlux-Regular.ttf"),
+    AfSemiBold: require("../assets/fonts/AfacadFlux-SemiBold.ttf"),
+    AfThin: require("../assets/fonts/AfacadFlux-Thin.ttf"),
+    AfExtraBold: require("../assets/fonts/AfacadFlux-ExtraBold.ttf"),
+    AfExtraLight: require("../assets/fonts/AfacadFlux-ExtraLight.ttf"),
+    AfLight: require("../assets/fonts/AfacadFlux-Light.ttf"),
   });
 
   useEffect(() => {
@@ -27,7 +38,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
